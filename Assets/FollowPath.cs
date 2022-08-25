2292a24a47243bb9ad1c2eaec47620910c3d0cc5
Roy <1141132211@qq.com>
2022-08-25 16:37:19 +0800

    first commit

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+    public GameObject wpManager;
+    GameObject[] wps;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start() {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    public void GoToHeli() {
+        agent.SetDestination(wps[4].transform.position);
+        // g.AStar(currentNode, wps[4]);
+        // currentWP = 0;
+    }
+
+    public void GoToRuin() {
+        agent.SetDestination(wps[0].transform.position);
+        // g.AStar(currentNode, wps[0]);
+        // currentWP = 0;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+
+    }
+}
