d6810549dc9f02a23ce3e2b5eab96ee71d3f8549
Roy <1141132211@qq.com>
2022-08-31 13:32:53 +0800

    tank movement added

--- Assets/FollowPath.cs
+++ Assets/FollowPath.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 
-public class FollowPath : MonoBehaviour {
+public class FollowPath : MonoBehaviour
+{
 
     public GameObject wpManager;
     GameObject[] wps;
     UnityEngine.AI.NavMeshAgent agent;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         wps = wpManager.GetComponent<WPManager>().waypoints;
         agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
-
-    public void GoToHeli() {
+    void Update()
+    {
+        float rh = Input.GetAxis("Horizontal");
+        float rv = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, rv) * Time.deltaTime * 5);
+        transform.Rotate(new Vector3(0, rh, 0) * Time.deltaTime * 20);
+    }
+    public void GoToHeli()
+    {
         agent.SetDestination(wps[4].transform.position);
     }
 
-    public void GoToRuin() {
+    public void GoToRuin()
+    {
         agent.SetDestination(wps[0].transform.position);
     }
 
     // Update is called once per frame
-    void LateUpdate() {
+    void LateUpdate()
+    {
 
     }
 }
