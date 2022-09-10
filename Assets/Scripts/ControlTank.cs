dd0c39e237f37d46de938b074e3206f4e4cebe06
Yuxuan Wang <yuxuan.www@gmail.com>
2022-09-10 22:29:14 +0800

    add acceleration buff ring

--- Assets/Scripts/ControlTank.cs
+++ Assets/Scripts/ControlTank.cs
@@ -8,6 +8,11 @@ public class ControlTank : MonoBehaviour
     UnityEngine.AI.NavMeshAgent agent;
     public float moveSpeed;
     public float rotationSpeed;
+
+    public float moveSpeedOri;
+    public float moveSpeedAcc;
+    public float accCount;
+
     void Start()
     {
         wps = wpManager.GetComponent<WPManager>().waypoints;
@@ -16,6 +21,18 @@ public class ControlTank : MonoBehaviour
     }
     void Update()
     {
+        // Acceleration Buff Counting
+        if (accCount > 0)
+        {
+            moveSpeed = moveSpeedAcc;
+            accCount -= Time.deltaTime;
+        }
+        else
+        {
+            moveSpeed = moveSpeedOri;
+            accCount = 0;
+        }
+
         float rh = Input.GetAxis("Horizontal");
         float rv = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(0, 0, rv) * Time.deltaTime * moveSpeed);
@@ -29,5 +46,17 @@ public class ControlTank : MonoBehaviour
     public void GoToRuin()
     {
         agent.SetDestination(wps[0].transform.position);
+    }
+
+    // Buff System
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Accelerate":
+                Destroy(other.gameObject);
+                accCount=15;
+                break;
+        }
     }
 }
