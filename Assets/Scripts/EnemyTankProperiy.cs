ef674f2fbd540c67642175fea0ec4234435773cc
GOOZW <goozwjenny@gmail.com>
2022-09-18 14:19:46 +0800

    add enemytank death effect

--- Assets/Scripts/EnemyTankProperiy.cs
+++ Assets/Scripts/EnemyTankProperiy.cs
@@ -10,14 +10,21 @@ public class EnemyTankProperiy : MonoBehaviour
     public float rotationSpeed;
     public float health;
     public float score;
+    public GameObject deathEffect;
     
     [SerializeField]
     private Slider healthSlider;
 
+    private float time;
+    private float timeDelay;
+
     void Start()
     {
         agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        time = 0f;
+        timeDelay = 3f;
         score = health;
+
     }
 
     void Update()
@@ -26,13 +33,27 @@ public class EnemyTankProperiy : MonoBehaviour
         healthSlider.value = health;
         if (health <= 0)
         {
-            Destroy(gameObject);
-            GameObject.Find("MainControl").GetComponent<MainControl>().gameScore += score;
-            GameObject.Find("MainControl").GetComponent<MainControl>().refreshCD = 5;
+
+            if(time == 0f){
+                GameObject turrent = this.transform.Find("TankRenderers").gameObject.transform.Find("TankTurret").gameObject;
+                turrent.AddComponent(typeof(Rigidbody));
+                turrent.AddComponent(typeof(BoxCollider));
+                Rigidbody turrentrigid = turrent.GetComponent<Rigidbody>();
+                turrentrigid.AddTorque(0, 0.05f, 0, ForceMode.Impulse);
+            } 
+            time = time + 1f*Time.deltaTime;
+            if(time > timeDelay){
+                time = 0f;
+                Instantiate(deathEffect, this.transform.position, this.transform.rotation);
+                Destroy(gameObject);
+                Destroy(gameObject);
+                GameObject.Find("MainControl").GetComponent<MainControl>().gameScore += score;
+                GameObject.Find("MainControl").GetComponent<MainControl>().refreshCD = 5;
+            }   
         }
 
 
     }
+}
 
    
-}
